7d776042f0f43a0f382d57b50c484c8fe490fa92
Corneliousm808 <corneliousmooring@gmail.com>
2016-11-02 08:15:11 -1000

    finally

---
+++ javaskript.js/qwl;re.js
@@ -0,0 +1,71 @@
+// this is a comment
+/* this is a block comment */
+
+// = is an assignment
+
+// > greater than
+// < less than
+// && and
+// || or
+
+var myName = "Jon";
+
+// this is an integer
+var myAge = 15
+
+// boolean type
+var boleanExample = true;
+
+// unassigned variable
+undefined
+
+// not a number
+NaN 
+
+// array
+
+// objects
+
+var camelCase = "yay"
+
+var firstName = "Cornelious";
+var lastName = "Mooring";
+var age = "15"
+
+
+console.log(firstName +" "+lastName + " is " + age)
+
+// function is an execute statement
+function (){
+
+}
+
+function nameOfFunction (){
+  console.log('hello I am a function')
+}
+
+//invoking a function
+nameOfFunction();
+
+//parameters
+function hello (a, b){
+  console.log(a + b)
+}
+
+hello(10,11)
+
+function createGreating(name, age){
+  if(age < 32)
+    console.log('hello ' + name);
+    return 'hello ' + name;
+  }else{
+    console.log('hello Mr. ' + name);
+    return 'hello Mr. ' + name;
+    return 'hello MRS. ' + name;
+  }
+
+
+var firstName = Kyle
+
+// creating an array
+var myArray = ['one', 2, 'seven', 'batman', true]
